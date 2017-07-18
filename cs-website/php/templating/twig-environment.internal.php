438d7da07e0d9688821cfd37c132ec4183fa3931
T0astBread <m.briedl@live.at>
2017-07-18 13:32:42 +0200

    Added Section Components & Automatic Labeling of Sections
    This commit adds the section component which is used to structure the page and display content as well as the section-group component (you can find more details on it in the corresponding *.twig files). I might abstractify the section component in the future (i.e. remove the necessity for a title).

--- cs-website/php/templating/twig-environment.internal.php
+++ cs-website/php/templating/twig-environment.internal.php
@@ -5,5 +5,14 @@ include $pathToRoot."vendor/autoload.php";
 $loader = new Twig_Loader_Filesystem("templates/");
 $loader->addPath("templates/components/");
 $loader->addPath("templates/pages/");
+
+$identifierFilter = new Twig_SimpleFilter("identifier", function($string, $group)
+{
+    $string = mb_convert_encoding($string, "iso-8859-1", "UTF-8");
+    $group = mb_convert_encoding($group, "iso-8859-1", "UTF-8");
+    return strtolower(($group !== "" ? $group."-" : "").str_replace(" ", "-", str_replace("Ä", "ae", str_replace("Ö", "oe", str_replace("Ü", "ue", strtoupper($string))))));
+});
+
 $twig = new Twig_Environment($loader);
+$twig->addFilter($identifierFilter);
 ?>
