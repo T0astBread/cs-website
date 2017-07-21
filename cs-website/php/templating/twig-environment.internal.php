ab27b54e14a38673562c7e3aa261527bd898f612
T0astBread <m.briedl@live.at>
2017-07-21 15:25:02 +0200

    Fixed Text Encoding Issues In Identifier Filter
    I hate PHP

--- cs-website/php/templating/twig-environment.internal.php
+++ cs-website/php/templating/twig-environment.internal.php
@@ -4,13 +4,14 @@ include $pathToRoot."vendor/autoload.php";
 
 $loader = new Twig_Loader_Filesystem("templates/");
 $loader->addPath("templates/components/");
+$loader->addPath("templates/components/assembled-components");
 $loader->addPath("templates/pages/");
 
 $identifierFilter = new Twig_SimpleFilter("identifier", function($string, $group)
 {
-    $string = mb_convert_encoding($string, "iso-8859-1", "UTF-8");
-    $group = mb_convert_encoding($group, "iso-8859-1", "UTF-8");
-    return strtolower(($group !== "" ? $group."-" : "").str_replace(" ", "-", str_replace("Ä", "ae", str_replace("Ö", "oe", str_replace("Ü", "ue", strtoupper($string))))));
+    // $string = mb_convert_encoding($string, "iso-8859-1", "UTF-8");
+    // $group = mb_convert_encoding($group, "iso-8859-1", "UTF-8");
+    return mb_strtolower(($group !== "" ? $group."-" : "").str_replace(" ", "-", str_replace("Ã„", "ae", str_replace("Ã–", "oe", str_replace("Ãœ", "ue", mb_strtoupper($string))))));
 });
 
 $twig = new Twig_Environment($loader);
