caa1fb6dfb0056cfd37e413bf868bd122159db62
T0astBread <m.briedl@live.at>
2017-07-28 17:03:11 +0200

    Implemented Helper Function for Version Number Conversion
    Implemented a helper function that converts version numbers stored in the database to nicely-formatted readable version numbers.

--- cs-website/php/utils.internal.php
+++ cs-website/php/utils.internal.php
@@ -9,4 +9,22 @@ function bad_request(string $response)
 {
     respond(400, $response);
 }
+
+function convert_db_response_to_version_number(int $dbResponse)
+{
+    $str = strval($dbResponse);
+    while(strlen($str) < 9)
+    {
+        $str = "0".$str;
+    }
+
+    $tokens = str_split($str, 3);
+    $assembledStr = "";
+    foreach($tokens as $token)
+    {
+        $assembledStr .= preg_replace("/^0{1,2}/", "", $token).".";
+    }
+
+    return substr($assembledStr, 0, strlen($assembledStr) - 1);
+}
 ?>
