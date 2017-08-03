15137a27316ea87b950111e196765a6c6bed2d65
T0astBread <m.briedl@live.at>
2017-08-03 17:01:03 +0200

    Changed A LOT of Things
    Well, my boss was here and he told me to change a few things. I hope that it doesn't stay that way.

--- cs-website/scripts/slide-with.ts
+++ cs-website/scripts/slide-with.ts
@@ -2,7 +2,9 @@ $(document).ready(() =>
 {
 	$(".slide-with").each((i, slideW) =>
 	{
-		let linked = $(".slideshow#" + slideW.getAttribute("x-slideshow"));
+		let sldShow = slideW.getAttribute("x-slideshow");
+		if(!sldShow || sldShow === "") return;
+		let linked = $(".slideshow#" + sldShow);
 		let id = getIdentifier(slideW);
 		slideW.setAttribute("id", id);
 		let linkedAttr = linked.attr("x-linked-slideshows");
