376f3b0c82e6649b99ebc85a42cbd70d59893a88
T0astBread <m.briedl@live.at>
2017-08-07 12:04:58 +0200

    Small Fix in Welcome Section for Firefox
    Now the slide-with in the welcome section starts off at the right height (with a small animation, but that doesn't matter) in Firefox. Previously, there used to be a small bug where it was, well, small in height.

--- cs-website/scripts/welcome.ts
+++ cs-website/scripts/welcome.ts
@@ -2,4 +2,5 @@ $(document).ready(() =>
 {
     let welcomeSlider = $("#welcome .slide-with");
     setAutoSlide(welcomeSlider[0], 4500);
+    updateLayout(welcomeSlider, welcomeSlider.find(".visible"));
 });
