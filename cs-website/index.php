34fbd57d4699e364dc4e4ef56967b092bb6f094a
T0astBread <m.briedl@live.at>
2017-08-09 09:56:30 +0200

    Fixed a Small Issue regarding the Initial Loading of the News
    Previously, the page used to throw a 400 error when no language was specified in the URL. This is now fixed.

--- cs-website/index.php
+++ cs-website/index.php
@@ -7,6 +7,7 @@ require_once "php/templating/page-renderer.internal.php";
 $_GET["product"] = "Allgemein";
 $_GET["limit"] = 7;
 $_GET["offset"] = 0;
+if(!isset($_GET["lang"])) $_GET["lang"] = "de";
 require_once "php/news-list.php";
 echo render_page("pages/home.html.twig", ["newsListLoaded" => ob_get_clean()]);
 ?>
