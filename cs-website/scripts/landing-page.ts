729173fe11c9df40d7a001326e8e81868811f788
T0astBread <m.briedl@live.at>
2017-08-11 09:52:35 +0200

    Added Different Options for Showing the Landing Page
    The landing page can now be shown daily, always or never. (See landingpage/index.html for more information.)
    
    This uses cookies so I probably have to add one of these cookie-bars.

--- cs-website/scripts/landing-page.ts
+++ cs-website/scripts/landing-page.ts
@@ -20,8 +20,8 @@ let resizeIFrameAccordingToSizeDefinition = (iFrame: HTMLIFrameElement) =>
 {
     let jqIf = $(iFrame);
     let head = $(iFrame.contentDocument.head);
-    let heightDef = head.find("meta[name=x-parent-height]");
-    let widthDef = head.find("meta[name=x-parent-width]");
+    let heightDef = head.find("meta[name=x-height]");
+    let widthDef = head.find("meta[name=x-width]");
     if(heightDef.length > 0) jqIf.parent().css("height", heightDef.attr("content"));
     if(widthDef.length > 0) jqIf.parent().css("width", widthDef.attr("content"));
 }
@@ -42,13 +42,42 @@ let connectStylesheetsToLandingPage = (landingPage: HTMLIFrameElement, ...styles
     stylesheetNames.forEach(s => connectStylesheetToLandingPage(head, s));
 }
 
+let checkIfCookieExistsAndPutCookie = () =>
+{
+    let cookie = document.cookie.match(/hasSeenLandingPage/);
+    if(cookie && cookie.length > 0) return true;
+
+    let expiryDate = new Date();
+    expiryDate.setTime(expiryDate.getTime() + 24*60*60*1000);
+    expiryDate.setHours(0);
+    expiryDate.setMinutes(0);
+    expiryDate.setSeconds(0);
+    document.cookie = "hasSeenLandingPage; expires=" + expiryDate.toUTCString() + ";";
+    return false;
+}
+
+let checkIfLandingPageExists = (landingPage: HTMLIFrameElement) =>
+{
+    let rMatch = landingPage.contentDocument.body.innerHTML.match(/(<h2>Error 404<\/h2>)|(File Not Found)/);
+    return rMatch === null || rMatch.length > 0;
+}
+
+let getShowMeta = (landingPageHead: JQuery) =>
+{
+    let showMeta = landingPageHead.find("meta[name=x-show]");
+    if(!showMeta || showMeta.length === 0) return "auto";
+    return showMeta.attr("content");
+}
+
 let initLandingPageContainers = () =>
 {
     let landingPage = document.getElementById("landing-page-container") as HTMLIFrameElement;
     landingPage.onload = () =>
     {
-        let rMatch = landingPage.contentDocument.body.innerHTML.match(/(<h2>Error 404<\/h2>)|(File Not Found)/);
-        if(rMatch && rMatch.length > 0) return;
+        let head = $(landingPage.contentDocument.head);
+        let showMeta = getShowMeta(head);
+        if(showMeta === "never" || !checkIfLandingPageExists(landingPage)) return;
+        if(showMeta !== "always" && checkIfCookieExistsAndPutCookie()) return;
 
         let jqLandingPage = $(landingPage);
         resizeIFrameAccordingToSizeDefinition(landingPage);
@@ -56,7 +85,7 @@ let initLandingPageContainers = () =>
         let title = landingPage.contentDocument.getElementsByTagName("title")[0];
         if(title) setLightboxTitle(jqLandingPage.closest(".lightbox")[0], title.innerText);
              
-        if($(landingPage.contentDocument.head).find("meta[name=x-no-default-styles]").length === 0) connectStylesheetsToLandingPage(landingPage, "frame", "misc");
+        if(head.find("meta[name=x-no-default-styles]").length === 0) connectStylesheetsToLandingPage(landingPage, "frame", "misc");
 
         showComponent(jqLandingPage.closest(".overlay")[0]);
     }
