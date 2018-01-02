dc78886111ca0f69da4bf6883f17bd2f39088af2
T0astBread <m.briedl@live.at>
2018-01-02 19:05:50 +0100

    Enabled gzip Compression on HTML

--- cs-website/php/templating/page-renderer.internal.php
+++ cs-website/php/templating/page-renderer.internal.php
@@ -4,6 +4,7 @@ require_once realpath(__DIR__)."/../jobs-count.internal.php";
 
 function render_page(string $pageName, $variables = [])
 {
+    ob_start("ob_gzhandler");
     return render($pageName, array_merge(["jobsCount" => get_jobs_count()], $variables));
 }
 ?>
