4adfcf5a8849c41ef45944a14b7c213da4ddffae
T0astBread <m.briedl@live.at>
2018-01-02 19:04:59 +0100

    Fixed new Imprint Page Links

--- cs-website/imprint/index.php
+++ cs-website/imprint/index.php
@@ -1,8 +1,8 @@
 <?php
-require_once "php/constants.internal.php";
+$pathToRoot = "../";
 
-$pathToRoot = "";
-include "php/templating/page-renderer.internal.php";
+require_once $pathToRoot."php/constants.internal.php";
+include $pathToRoot."php/templating/page-renderer.internal.php";
 
 echo render_page("pages/imprint.html.twig");
 ?>
